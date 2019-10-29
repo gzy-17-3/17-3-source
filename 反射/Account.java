b9dad546d2ae755402117f9e24dc22244ee0455f
LXF <rdxer@foxmail.com>
2019-10-29 22:53:33 +0800

    Create Account.java

---
+++ 反射/Account.java
@@ -0,0 +1,31 @@
+package com.company;
+
+public class Account {
+    private String name;
+    private String pwd;
+    private int age;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPwd() {
+        return pwd;
+    }
+
+    public void setPwd(String pwd) {
+        this.pwd = pwd;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+}
